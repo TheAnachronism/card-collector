c67a77c8f1e01e801cea278d23b08a92b5d136f3
TheAnachronism <theanachronism@outlook.com>
2025-10-13 18:07:48 +0200

    Start work on general app layout and theming.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,23 @@
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "shadcn-nuxt", "convex-nuxt"],
-  css: ['~/assets/css/tailwind.css'],
+  modules: ["@nuxt/eslint", "shadcn-nuxt", "convex-nuxt", "nuxt-lucide-icons"],
+  css: ["~/assets/css/tailwind.css"],
+  alias: {
+    "@": resolve(__dirname, "./app/"),
+  },
   convex: {
     url: process.env.CONVEX_URL,
   },
   shadcn: {
-    prefix: '',
-    componentDir: '~/components/ui',
+    prefix: "",
+    componentDir: "~/components/ui",
+  },
+  lucide: {
+    namePrefix: "Icon",
   },
   vite: {
     plugins: [tailwindcss()],
